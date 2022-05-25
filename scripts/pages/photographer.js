966590ddd5d19e7f918a8b9db098143546c2882b
Rogalio mouchel <you@example.com>
2022-05-25 16:44:28 +0200

    add photgrapher

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,11 +1,13 @@
-//Mettre le code JavaScript lié à la page photographer.html
-//on cherche url + enleve ?
-const ID = window.location.search.substring(1);
-console.log(ID);
+async function getPhotographer() {
+  //on cherche url + enleve ?
+  const ID = window.location.search.substring(1);
+  console.log(ID);
 
-const PhotographURL = "data/photographers.json";
+  // URL
+  const PhotographURL = "data/photographers.json";
 
-let response = await fetch(PhotographURL);
-// parse la reponse
-let data = await response.json();
-return data;
+  let response = await fetch(PhotographURL);
+  // parse la reponse
+  let data = await response.json();
+  return data;
+}
