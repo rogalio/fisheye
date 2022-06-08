67008992764634552a007937d802aeb37a35600d
Rogalio <rogalio@macbook-pro-de-rogalio.home>
2022-06-08 17:51:41 +0200

    add likes

--- scripts/factories/Picture.js
+++ scripts/factories/Picture.js
@@ -12,6 +12,12 @@ class Picture {
     const div = document.createElement("div");
     const title = document.createElement("p");
     const image = document.createElement("img");
+    const number = document.createElement("span");
+    const like = document.createElement("i");
+    const numOfLikes = document.createTextNode(this.likes);
+    // transfirler en nombres
+    document.getElementById("likeBar").textContent =
+      Number(document.getElementById("likeBar").textContent) + this.likes;
     div.appendChild(title);
     div.appendChild(image);
     div.classList.add("allMedia");
@@ -20,6 +26,16 @@ class Picture {
     //remplacer image par asset/....
     image.setAttribute("src", `assets/images/${this.image}`);
     image.setAttribute("alt", this.title);
+    // creer nombre  + coeur
+    div.appendChild(number);
+    number.appendChild(like);
+    // ajout click
+    number.addEventListener("click", () => {
+      this.likes = this.likes + 1;
+      numOfLikes.textContent = this.likes;
+    });
+    number.appendChild(numOfLikes);
+    like.classList.add("fa-solid", "fa-heart");
     return div;
   }
 }
--- scripts/factories/Video.js
+++ scripts/factories/Video.js
@@ -13,6 +13,12 @@ class Video {
     const title = document.createElement("p");
     const video = document.createElement("video");
     const source = document.createElement("source");
+    const number = document.createElement("span");
+    const like = document.createElement("i");
+    const numOfLikes = document.createTextNode(this.likes);
+    // transfirler en nombres
+    document.getElementById("likeBar").textContent =
+      Number(document.getElementById("likeBar").textContent) + this.likes;
     div.appendChild(title);
     div.appendChild(video);
     div.classList.add("allMedia");
@@ -20,8 +26,19 @@ class Video {
     div.appendChild(source);
     title.textContent = this.title;
     //remplacer image par asset/....
+    video.append(source);
     source.setAttribute("src", `assets/images/${this.video}`);
     source.setAttribute("type", "video/mp4");
+    // creer nombre  + coeur
+    div.appendChild(number);
+    number.appendChild(like);
+    // ajout click
+    number.addEventListener("click", () => {
+      this.likes = this.likes + 1;
+      numOfLikes.textContent = this.likes;
+    });
+    number.appendChild(numOfLikes);
+    like.classList.add("fa-solid", "fa-heart");
     return div;
   }
 }
