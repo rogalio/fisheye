29682b65811c74a985433b885752b3853e28edb5
Rogalio <rogalio@macbook-pro-de-rogalio.home>
2022-07-13 18:10:12 +0200

    h

--- scripts/utils/LightBox.js
+++ scripts/utils/LightBox.js
@@ -1,6 +1,30 @@
 class Lightbox {
   static medias = [];
   constructor(id) {
+    // lightbox keyDown
+    window.addEventListener("keydown", (event) => {
+      if (event.keyCode === 27) {
+        modalLightBox.style.display = "none";
+      } else if (event.keyCode === 37) {
+        this.index =
+          (((this.index - 1) % Lightbox.medias.length) +
+            Lightbox.medias.length) %
+          Lightbox.medias.length;
+        document.getElementById("customInfo").innerHTML = "";
+        document
+          .getElementById("customInfo")
+          .appendChild(Lightbox.medias[this.index].createCardLightBox());
+      } else if (event.keyCode === 39) {
+        this.index =
+          (((this.index - 1) % Lightbox.medias.length) +
+            Lightbox.medias.length) %
+          Lightbox.medias.length;
+        document.getElementById("customInfo").innerHTML = "";
+        document
+          .getElementById("customInfo")
+          .appendChild(Lightbox.medias[this.index].createCardLightBox());
+      }
+    });
     this.index = Lightbox.medias.findIndex((e) => {
       return e.id == id;
     });
@@ -23,17 +47,13 @@ class Lightbox {
     // aller a gauche
     const left = document.getElementById("left");
     left.addEventListener("click", () => {
-      if (this.index === 0) {
-        this.index = this.index.length;
-        left.style.display = "none";
-      } else {
-        left.style.display = "flex";
-        this.index = (this.index - 1) % Lightbox.medias.length;
-        document.getElementById("customInfo").innerHTML = "";
-        document
-          .getElementById("customInfo")
-          .appendChild(Lightbox.medias[this.index].createCardLightBox());
-      }
+      this.index =
+        (((this.index - 1) % Lightbox.medias.length) + Lightbox.medias.length) %
+        Lightbox.medias.length;
+      document.getElementById("customInfo").innerHTML = "";
+      document
+        .getElementById("customInfo")
+        .appendChild(Lightbox.medias[this.index].createCardLightBox());
     });
 
     // fermer le lightgox
